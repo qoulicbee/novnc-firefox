7ab8556b364f7b72953285389eb2849e807cd85a
qoulicbee <43243161+qoulicbee@users.noreply.github.com>
2018-09-14 12:11:59 +0800

    Create checkfire.sh

---
+++ checkfire.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+check_process() {
+  echo "$ts: checking $1"
+  [ "$1" = "" ]  && return 0
+  [ `pgrep -n $1` ] && return 1 || return 0
+}
+
+while [ 1 ]; do
+  # timestamp
+  ts=`date +%T`
+
+  echo "$ts: begin checking..."
+  check_process "firefox"
+  [ $? -eq 0 ] && echo "$ts: not running, restarting..." && `firefox &`
+  sleep 5
+done
