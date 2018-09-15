fabfc6b29240856984d0833ac35a9905de684f5d
qoulicbee <43243161+qoulicbee@users.noreply.github.com>
2018-09-15 11:16:17 +0800

    Update vncmain.sh

--- vncmain.sh
+++ vncmain.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
-sudo -u user /home/user/checkfire.sh &
-sudo -u root /home/user/checkdefun.sh &
+
+check_process() {
+  echo "$ts: checking $1"
+  [ "$1" = "" ]  && return 0
+  [ `pgrep -n $1` ] && return 1 || return 0
+}
+
+killDefunct() {
+    for pid in $(ps axo pid=,stat= | awk '$2~/^Z/ { print $1 }') ; do
+        echo "$pid" # do something interesting here
+        sudo kill -9 $pid
+    done
+}
+
+
+while [ 1 ]; do
+  # timestamp
+  ts=`date +%T`
+  echo "$ts: begin checking..."
+  killDefunct
+  check_process "firefox"
+  [ $? -eq 0 ] && echo "$ts: not running, restarting..." && `firefox &`
+  sleep 30
+done
 xterm
