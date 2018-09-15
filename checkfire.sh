3ca06a0c3da08b4827c57207cb4afe7ddeeac778
qoulicbee <43243161+qoulicbee@users.noreply.github.com>
2018-09-15 10:40:52 +0800

    Update checkfire.sh

--- checkfire.sh
+++ checkfire.sh
@@ -5,17 +5,10 @@ check_process() {
   [ "$1" = "" ]  && return 0
   [ `pgrep -n $1` ] && return 1 || return 0
 }
-
-killDefunct() {
-    for pid in $(ps axo pid=,stat= | awk '$2~/^Z/ { print $1 }') ; do
-        echo "$pid" # do something interesting here
-        sudo kill -9 $pid
-    done
-}
-
-ts=`date +%T`
-echo "$ts: begin checking..."
-killDefunct
-check_process "firefox"
-[ $? -eq 0 ] && echo "$ts: not running, restarting..." && `firefox &`
-sleep 5
+while [ 1 ]; do
+    ts=`date +%T`
+    echo "$ts: begin checking..."
+    check_process "firefox"
+    [ $? -eq 0 ] && echo "$ts: not running, restarting..." && `firefox &`
+    sleep 5
+done
